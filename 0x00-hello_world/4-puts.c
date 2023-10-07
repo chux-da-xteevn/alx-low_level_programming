ed1e8872a4abedcb7c53c20afa9219935e3191f3
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-10-07 08:23:20 -0700

    4th commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - controls the entire code functionality
+ *
+ * Return: success means 0 always
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
