0912c3e5f201f8085d5190862327e87335f12a94
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-10-07 08:36:59 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - in charge of the core functions
+ *
+ * Return: has 0 for success at all times
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return  (0);
+}
