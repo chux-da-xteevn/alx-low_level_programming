164d4fd260daeab0542ff6a64b4eb9f6ec1be787
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-10-10 16:54:52 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,15 +4,14 @@
  * print_alphabet - Entry point
  * Return: 0
  */
-
 void print_alphabet(void)
 {
-    char c = 'a';
+	char c = 'a';
 
-    while (c <= 'z')
-    {
-        _putchar(c);
-        c++;
-    }
-    _putchar('\n');
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 }
