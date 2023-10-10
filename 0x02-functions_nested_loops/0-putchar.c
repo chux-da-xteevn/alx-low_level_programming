327afb2cf25747537ea156c958ad3ed7e0f716d1
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-10-10 15:49:18 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- *
- * main - carries the progrm
- *
- * return : returns 0 for success
- *
- */
+ *  * main -  prints _putchar, followed by a new line
+ *   * Return: Always 0 (Success)
+ *    */
+int main(void)
+{
+		_putchar('_');
+			_putchar('p');
+				_putchar('u');
+					_putchar('t');
+						_putchar('c');
+							_putchar('h');
+								_putchar('a');
+									_putchar('r');
+										_putchar('\n');
 
-int _putchar(char c) {
-  
-}
-
-int main() {
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
-  return 0;
+											return (0);
 }
