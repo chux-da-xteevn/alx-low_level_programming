c1ecd1cb27eaa7313fb20962c60e9da23cbd1bbd
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-10-14 07:52:10 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,19 +5,18 @@
  */
 void more_numbers(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 15; j++)
-        {
-            if (j > 9)
-            {
-                _putchar('0' + (j / 10)); /* Print the tens digit */
-            }
-            _putchar('0' + (j % 10)); /* Print the ones digit */
-        }
-        _putchar('\n'); /* New line after each set of numbers */
-    }
+	for (i = 0; i < 10; i++)
+	
+	for (j = 0; j < 15; j++)
+	{
+		if (j > 9)
+		{
+			_putchar('0' + (j / 10)); /* Print the tens digit */
+		}
+		_putchar('0' + (j % 10)); /* Print the ones digit */
+	}
+	_putchar('\n'); /* New line after each set of numbers */
 }
 
