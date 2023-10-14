24efd789442cbc00c6284c1e2cfcaef8b7cf2499
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-10-14 08:23:14 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,11 +12,11 @@ void print_line(int n)
 	}
 	else
 	{
-	int i;
-	for (i = 0; i < n; i++)
-	{
-		 _putchar('_');
-	}
-	_putchar('\n');
+		int i;
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
 }
