fabf376568258a20bb41d0afea9d0d3a50b9a343
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-10-14 10:07:59 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - powers the main code
+ *
+ * Return: Always 0 for (Success)
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            printf("FizzBuzz");
+        else if (i % 3 == 0)
+            printf("Fizz");
+        else if (i % 5 == 0)
+            printf("Buzz");
+        else
+            printf("%d", i);
+
+        if (i != 100)
+            printf(" ");
+        else
+            printf("\n");
+    }
+
+    return (0);
+}
