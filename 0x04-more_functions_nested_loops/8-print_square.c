232bb2368758366b59b8f03c2bbdaa0bf91e8457
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-10-14 09:59:04 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,7 +3,6 @@
 /**
  * print_square - prints a square, followed by a new line;
  * @size: size of the square
- *
  */
 void print_square(int size)
 {
@@ -14,13 +13,14 @@ void print_square(int size)
 	else
 	{
 		int i, j;
+
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
-			_putchar('ln');
+			_putchar('\n');
 		}
 	}
 }
