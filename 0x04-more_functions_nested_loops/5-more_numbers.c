06b7bcfe0eba6aa2005b48082d1c4aab5a28dfb9
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-10-14 07:45:37 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14, ten times
+ */
+void more_numbers(void)
+{
+    int i, j;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 15; j++)
+        {
+            if (j > 9)
+            {
+                _putchar('0' + (j / 10)); /* Print the tens digit */
+            }
+            _putchar('0' + (j % 10)); /* Print the ones digit */
+        }
+        _putchar('\n'); /* New line after each set of numbers */
+    }
+}
+
