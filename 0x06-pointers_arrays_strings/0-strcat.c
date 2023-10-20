b1d206aab6adefda44bb054f72c3f250afab52d5
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-10-19 20:16:01 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,19 +9,19 @@
  */
 char *_strcat(char *dest, const char *src)
 {
-    int dest_len = 0;
-    int i;
+	int dest_len = 0;
+	int i;
 
-    while (dest[dest_len] != '\0')
-    {
-        dest_len++;
-    }
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
 
-    for (i = 0; src[i] != '\0'; i++)
-    {
-        dest[dest_len + i] = src[i];
-    }
-    dest[dest_len + i] = '\0';
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
 
-    return dest;
+	return (dest);
 }
