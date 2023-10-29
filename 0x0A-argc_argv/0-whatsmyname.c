f7b3fc9a74f42ba3b6acfad61719a7c0505f3d6d
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-10-29 08:19:16 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include "main.h"
 
-int main(int argc, char *argv[]) {
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+
+	{
 	if (argc >= 1)
 	{
-		printf("%s\n", argv[0]);
+		printProgramName(argv[0]);
 	}
 	return (0);
 }
---
+++ 0x0A-argc_argv/printProgramName.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void printProgramName(char *programName)
+{
+	printf("%s\n", programName);
+}
