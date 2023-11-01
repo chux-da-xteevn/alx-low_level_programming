0cf478b75e6afc86a27f2dc6b500dc1abb2fe903
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-11-01 13:15:50 -0700

    ntask 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,35 +11,35 @@
  */
 char *argstostr(int ac, char **av)
 {
-    int i, j, total_len;
-    char *str, *str_ptr;
+	int i, j, total_len;
+	char *str, *str_ptr;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-    total_len = 0;
+	total_len = 0;
 
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-            total_len++;
-        total_len++; /* Add space for newline character */
-    }
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			total_len++;
+		total_len++;
+	}
 
-    str = malloc(total_len + 1);
-    if (str == NULL)
-        return (NULL);
+	str = malloc(total_len + 1);
+	if (str == NULL)
+		return (NULL);
 
-    str_ptr = str;
+	str_ptr = str;
 
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-            *str_ptr++ = av[i][j];
-        *str_ptr++ = '\n'; /* Add newline character */
-    }
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			*str_ptr++ = av[i][j];
+		*str_ptr++ = '\n'; /* Add newline character */
+	}
 
-    *str_ptr = '\0'; /* Null-terminate the string */
+	*str_ptr = '\0'; /* Null-terminate the string */
 
-    return (str);
+	return (str);
 }
