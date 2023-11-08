16c2ba60f370aef695b8a51072011634204f4704
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-11-08 07:52:53 -0800

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - Define a new struct dog type
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+typedef struct dog
+{
     char *name;
     float age;
     char *owner;
-};
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+} dog_t;
 
-
-#endif
+#endif /* DOG_H */
