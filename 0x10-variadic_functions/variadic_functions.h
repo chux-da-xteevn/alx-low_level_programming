74df64be2efd46bbf53e86e263728fc8ec619bd4
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-11-09 13:35:32 -0800

    task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -20,5 +20,6 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
