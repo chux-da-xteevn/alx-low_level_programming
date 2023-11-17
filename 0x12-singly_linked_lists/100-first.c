14f90f84959e60dc7503e681e0d1c90b19b1282f
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-11-16 16:12:50 -0800

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * premain - This Function executes before the main function.
+ *
+ * Description: Prints a message before the main function is executed.
+ */
+void __attribute__((constructor)) premain(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
